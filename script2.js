fa5fad71c424c3eaf1c6133b996e780cfa6966c0
S Jones <siouxgeonz@gmail.com>
2019-12-13 15:42:38 +0000

    In initial, the button change works in .js2  *does not throw error.*   It now makes all items yellow.

--- script2.js
+++ script2.js
@@ -1,8 +1,18 @@
-changeBackgrounds('.button1','yellow');
+// changeBackgrounds('.button1','yellow');
+// function changeBackgrounds(selectorStr, bgColor) {
+//   let newColor=bgColor;
+//   let thing=selectorStr;
+//  document.querySelectorAll('.button1').forEach(elem=>{elem.style.backgroundColor=newColor;});
+//  // document.querySelectorAll('.item').style.color=newColor;;
+//   //box.style.color=bgcolor;
+//   //document.getElementById("start").style.color=newColor;
+    
+// }
+changeBackgrounds('.item','yellow');
 function changeBackgrounds(selectorStr, bgColor) {
   let newColor=bgColor;
   let thing=selectorStr;
- document.querySelectorAll('.button1').forEach(elem=>{elem.style.backgroundColor=newColor;});
+ document.querySelectorAll('.item').forEach(elem=>{elem.style.backgroundColor=newColor;});
  // document.querySelectorAll('.item').style.color=newColor;;
   //box.style.color=bgcolor;
   //document.getElementById("start").style.color=newColor;
