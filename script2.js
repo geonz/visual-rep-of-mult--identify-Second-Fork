44217c3a93e745e757e55ce108357483f8b5ffe3
S Jones <siouxgeonz@gmail.com>
2019-12-13 16:27:21 +0000

    I made "change background" again.

--- script2.js
+++ script2.js
@@ -6,15 +6,14 @@ changeBackgrounds('section > div','yellow');
 function changeBackgrounds(selectorStr, bgColor) {
   
   let d = document.querySelectorAll(selectorStr); 
-  d[3].style.backgroundColor="orange";
-//  document.querySelectorAll('.item').forEach(elem=>{elem.style.backgroundColor=newColor;});
+
+  for (let n=factSet-1; n<factNum*factSet; n+=factSet)
+  
+ { d[n].style.backgroundColor="orange";}
+ 
 }
   
-  c = document.querySelectorAll('section > div');
-
-  c[0].style.color="pink";
-  // document.querySelectorAll('.item').style.color=newColor;;
-  //box.style.color=bgcolor;
-  //document.getElementById("start").style.color=newColor;
+   
+   
     
  
